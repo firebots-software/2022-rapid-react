658d2978594390532de4e8b0d3d48450aac8efac
msoham123 <msoham123@gmail.com>
2022-01-15 01:01:44 -0800

    Added comments for table object creation

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -10,8 +10,13 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Limelight extends SubsystemBase {
 
+  /*Get the default instance of NetworkTables that was created automatically
+  when your program starts */
   NetworkTableInstance instance = NetworkTableInstance.getDefault();
   
+  /*Get the table within that instance that contains the data. There can
+  be as many tables as you like and exist to make it easier to organize
+  your data. In this case, it's a table called datatable. */
   NetworkTable table = instance.getTable("limelight");
 
   /** Creates a new Limelight. */
