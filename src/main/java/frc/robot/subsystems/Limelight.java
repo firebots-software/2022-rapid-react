86608306f60c5095419ffd049bfdcd511cb15b3d
msoham123 <msoham123@gmail.com>
2022-01-20 20:38:50 -0800

    Removed tx, ty, tv variables and methods

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -20,40 +20,11 @@ public class Limelight extends SubsystemBase {
   your data. In this case, it's a table called datatable. */
   private NetworkTable table = instance.getTable("GRIP/myContoursReport");
 
-  // A network table entry that stores the tx value
-  private NetworkTableEntry tx;
-
-  // A network table entry that stores the ty value
-  private NetworkTableEntry ty;
-
-  // A network table entry that stores the tv value
-  private NetworkTableEntry tv;
-
-  // A network table entry that stores the ta value
-  private NetworkTableEntry ta;
-
   private NetworkTableEntry contourReport;
-
-  // The value of tx to be returned if no value is found
-  private final double DEFAULT_VALUE_TX = 0.0;
-
-  // The value of ty to be returned if no value is found
-  private final double DEFAULT_VALUE_TY = 0.0;
-
-  // The value of tv to be returned if no target is found
-  private final double DEFAULT_VALUE_TV = 0;
-
-  // The value of ta to be returned if no target is found
-  private final double DEFAULT_VALUE_TA = 0;
-
   
-
   /** Creates a new Limelight. */
   public Limelight() {
-    // tx.setDouble(DEFAULT_VALUE_TX);
-    // ty.setDouble(DEFAULT_VALUE_TY);
-    // tv.setDouble(DEFAULT_VALUE_TV);
-    // ta.setDouble(DEFAULT_VALUE_TA);
+    
   }
 
     /**
@@ -75,23 +46,12 @@ public class Limelight extends SubsystemBase {
   }
 
   /**
-   * Refreshes (updates) the tx and ty values
+   * Refreshes (updates) the myContoursReport Network Table
    */
   public void refreshValues(){
-    // tx = table.getEntry("tx");
-    // ty = table.getEntry("ty");
-    // tv = table.getEntry("tv");
-    // ta = table.getEntry("ta");
     table = instance.getTable("GRIP/myContoursReport");
   }
 
-   /**
-     * Returns the horizontal offset from the crosshair to the target (-27 degrees to 27 degrees).
-     * @return Horizontal offset in degrees.
-     */
-  public double getTx() {
-    return tx.getDouble(DEFAULT_VALUE_TX);
-  }
 
   public double[][] getContours(){
     double[] areaArr = table.getEntry("area").getDoubleArray(new double[]{}); 
@@ -104,31 +64,6 @@ public class Limelight extends SubsystemBase {
     return new double[][]{areaArr, centerXArr, centerYArr, widthArr, heightArr, solidityArr};
   }
 
-   /**
-     * Returns the vertical offset from the crosshair to the target (-20.5 degrees to 20.5 degrees). 
-     * @return Vertical offset in degrees.
-     */
-  public double getTy() {
-    return ty.getDouble(DEFAULT_VALUE_TY);
-  }
-
-
-   /**
-     * Returns the target area. (From 0% of the image to 100% of the image) 
-     * @return Target area as a percentage.
-     */
-    public double getTa() {
-      return ta.getDouble(DEFAULT_VALUE_TA);
-    }
-
-
-   /**
-     * Returns whether the Limelight has any valid targets (0 or 1). This means that the Limelight has a valid target, or is not. 
-     * @return Boolean value for if limelight has a target.
-     */
-    public boolean getTv() {
-      return tv.getDouble(DEFAULT_VALUE_TV) == 1;
-    }
 
     // Subsystem Calculations
     // public double getXOffset() {
