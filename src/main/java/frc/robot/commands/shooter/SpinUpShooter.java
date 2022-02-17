9117bfa0ad61a22db70d887eb86c97be6818d0f1
pjoshi23 <star.flight431@gmail.com>
2022-02-16 20:24:52 -0800

    added 2 pid controllers for the shooter in SpinUpShooter

--- src/main/java/frc/robot/commands/shooter/SpinUpShooter.java
+++ src/main/java/frc/robot/commands/shooter/SpinUpShooter.java
@@ -4,6 +4,10 @@
 
 package frc.robot.commands.shooter;
 
+import javax.swing.text.StyleContext.SmallAttributeSet;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.Shooter;
@@ -11,14 +15,22 @@ import frc.robot.subsystems.Shooter;
 public class SpinUpShooter extends CommandBase {
   // keep spinning after aimed and during load ball --> toggleFlywheel
   private final Shooter shooter;
-  private final double P = 0.001; //CONSTANT - magic number, figure out thru testing; should be pretty small
-
+  private final double P = 1/2000.0; //CONSTANT - magic number, figure out thru testing; should be pretty small
+  private final PIDController pidTop, pidBottom; 
+  private final double kp, ki, kd; 
 
   /** Creates a new SpinUpShooter. */
-  public SpinUpShooter(double speed) { //speed in RPM
+  public SpinUpShooter(double speed) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.shooter = Shooter.getInstance();
-    shooter.setTargetSpeed(speed); 
+    shooter.setTargetSpeed(speed);
+    kp = 1/2000; 
+    ki = 0; 
+    kd = 0; 
+    pidTop = new PIDController(kp, ki, kd); 
+    pidBottom = new PIDController(kp, ki, kd); 
+    pidTop.setSetpoint(shooter.getTargetSpeed());
+    pidBottom.setSetpoint(shooter.getTargetSpeed());
   }
 
   // Called when the command is initially scheduled.
@@ -28,27 +40,25 @@ public class SpinUpShooter extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double topError = shooter.getTargetSpeed() - shooter.getTopShooterRPM();; // desired = desired aimed position we want
-    double bottomError = shooter.getTargetSpeed() - shooter.getBottomShooterRPM();; // desired = desired aimed position we want
+    double outputTop = pidTop.calculate(shooter.getTopShooterRPM()); 
+    SmartDashboard.putNumber("shooter top motor output", outputTop); 
+    shooter.setTopMotorSpeed(outputTop); 
 
-    double newValTop = P * topError; //magic number P (proportionality constant)
-    double newValBottom = P * bottomError; 
-    shooter.setTopMotorSpeed(newValTop); //
-    shooter.setBottomMotorSpeed(newValBottom);
+    double outputBottom = pidBottom.calculate(shooter.getBottomShooterRPM()); 
+    SmartDashboard.putNumber("shooter bottom motor output", outputBottom); 
+    shooter.setBottomMotorSpeed(outputBottom);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    shooter.stopBottomMotor();
-    shooter.stopTopMotor();
+    
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
-    // return shooter.isAtTargetSpeed(Constants.Shooter.motorSpeedToleranceRPM);
+    return shooter.isAtTargetSpeed(Constants.Shooter.motorSpeedToleranceRPM);
         // possible return false and change it in the command group
         //  TODO: command group - if interrupted then return true --> loadBall
   }
