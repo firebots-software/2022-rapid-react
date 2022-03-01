5f6708828160f99fa45cd6ccccd1572b92070f0e
kteodorovich <katyateodorovich@gmail.com>
2022-03-01 11:53:52 -0800

    fixed commandGroup package naming & updated wpilib

--- src/main/java/frc/robot/commandGroups/StopEverything.java
+++ src/main/java/frc/robot/commandGroups/StopEverything.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commandgroups;
+package frc.robot.commandGroups;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.commands.shooter.StopShooter;
