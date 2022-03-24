ea59bf7eb57a85f90b2d78092e17c24c6fb8f3f5
vzaveri23 <virajsz1123@gmail.com>
2022-03-23 22:19:25 -0700

    Added pathweaver test trajectory by reading json

--- src/main/java/frc/robot/Paths.java
+++ src/main/java/frc/robot/Paths.java
@@ -4,6 +4,9 @@
 
 package frc.robot;
 
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.Path;
 import java.util.List;
 
 import edu.wpi.first.math.geometry.Pose2d;
@@ -11,10 +14,16 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.TrajectoryUtil;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
 
 /** Add your docs here. */
 public class Paths {
-    public static Trajectory moveToBall, moveToShootingDistanceFromBall;
+    public static Trajectory test;
+    public static Trajectory moveToBall, moveToShootingDistanceFromBall, testPath;
+    static String trajectoryJSON = "src/main/deploy/paths/Test.wpilib.json";
 
     public static void generate(){
         moveToBall = TrajectoryGenerator.generateTrajectory(
@@ -29,5 +38,14 @@ public class Paths {
             Constants.Drivetrain.MotionProfilingConfigReversed
         );
 
+        try {
+            Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
+            testPath = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+        } catch (IOException ex) {
+            DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
+        }
+
     }
+
+
 }
